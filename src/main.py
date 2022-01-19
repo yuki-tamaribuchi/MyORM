8bd98313b48a3b145d414e2aaf4c783b74f6639d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-19 12:12:04 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@ from models.users import Users
 #直接渡してデータをセットし、保存する方法
 #user=Users(username="yuki")
 #user.save()
+#print(user)
 
 
 #createメソッドにデータを渡し保存する方法
-#user = Users().objects.create(username="yuki", lucky_number=1)
-#print(user)
+user = Users().objects.create(username="yuki", lucky_number=1)
+print(user)
 
 
 #インスタンスのupdateメソッドで値を更新
@@ -21,6 +22,5 @@ from models.users import Users
 
 
 #インスタンスのgetメソッドでデータを取得
-user = Users().objects.create(username="yuki", lucky_number=1)
-
-user2 = Users().objects.get(username="yuki")
+#user = Users().objects.create(username="yuki", lucky_number=1)
+#user2 = Users().objects.get(username="yuki")
