673f14fcbae5c2f2e6f0f89b56804b2ab31da42e
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-19 11:08:34 +0900

    Modify update

--- src/framework/objects/update.py
+++ src/framework/objects/update.py
@@ -3,5 +3,8 @@ from .base import ObjectsBase
 
 
 class ObjectsUpdate(ObjectsBase):
-	def update(self):
-		pass
+	def update(self, **kwargs):
+		update_data = kwargs
+		self.model_instance.update_values(update_data)
+		self.model_instance.save()
+		return self.model_instance
