b2a69bb29960c823ff61891a73f4283b37e6bb1f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-20 00:09:59 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,30 @@
 from models.users import Users
 
 
+from models.entry import Entry
+from framework.sqls.create_table import generate_create_table_sql
+
+generate_create_table_sql(Entry)
+
+
+#entry=Entry(writer=1, title="sample")
+#entry.save()
+#print(entry.__dict__)
+
+
+
+
+
+#動作確認済分
+
+#テーブル作成 (作成するテーブルをmodel.__init__内のmodelsリストに追加しておく)
+#from framework.operates.create_table import create_all_table
+#create_all_table()
+
+
+
+#以下SQL発行は未実装
+
 #直接渡してデータをセットし、保存する方法
 #user=Users(username="yuki")
 #user.save()
@@ -8,8 +32,8 @@ from models.users import Users
 
 
 #createメソッドにデータを渡し保存する方法
-user = Users().objects.create(username="yuki", lucky_number=1)
-print(user)
+#user = Users().objects.create(username="yuki", lucky_number=1)
+#print(user)
 
 
 #インスタンスのupdateメソッドで値を更新
