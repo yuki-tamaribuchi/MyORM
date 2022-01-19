ee24bab4bda723a2a7c7dd33415c31b3570f543c
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-20 00:09:17 +0900

    Add CREATE_TABLE_TEMPLATE

--- src/framework/sqls/templates.py
+++ src/framework/sqls/templates.py
@@ -1,3 +1,5 @@
+CREATE_TABLE_TEMPLATE = "CREATE TABLE {table} ( {fields} )"
+
 SELECT_TEMPLATE = "SELECT {options} {columns} FROM {table} {join} {where} {order_by} {having} {limit}"
 INSERT_TEMPLATE = "INSERT INTO {table} ({columns}) VALUES ({values})"
 UPDATE_TEMPLATE = "UPDATE {table} SET {sets} {where}"
