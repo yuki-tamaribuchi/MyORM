9b47c82cf76076aa80d2291f8e7b072e3ac26dc7
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-19 11:06:44 +0900

    Modify model_class to model_instance

--- src/framework/objects/create.py
+++ src/framework/objects/create.py
@@ -4,7 +4,6 @@ from .base import ObjectsBase
 class ObjectsCreate(ObjectsBase):
 	def create(self, **kwargs):
 		input_data = kwargs
-		model_class_instance =self.model_class()
-		model_class_instance.set_values(input_data=input_data)
-		model_class_instance.save()
-		return model_class_instance
+		self.model_instance.set_values(input_data=input_data)
+		self.model_instance.save()
+		return self.model_instance
