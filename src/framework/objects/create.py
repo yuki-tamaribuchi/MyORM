b341f18b3c086ff5173df72d2dcccd908bd5ed01
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-19 10:11:16 +0900

    Add call set_values and save

--- src/framework/objects/create.py
+++ src/framework/objects/create.py
@@ -4,4 +4,7 @@ from .base import ObjectsBase
 class ObjectsCreate(ObjectsBase):
 	def create(self, **kwargs):
 		input_data = kwargs
-		self.model_class().set_values(input_data=input_data)
+		model_class_instance =self.model_class()
+		model_class_instance.set_values(input_data=input_data)
+		model_class_instance.save()
+		return model_class_instance
