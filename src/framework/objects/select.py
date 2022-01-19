2f0b6ebb8b46ae58cd7433556e161e1f8a3e154d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-19 11:25:16 +0900

    Implement get

--- src/framework/objects/select.py
+++ src/framework/objects/select.py
@@ -2,8 +2,11 @@ from .base import ObjectsBase
 
 
 class ObjectsSelect(ObjectsBase):
-	def get(self):
-		pass
+	def get(self, **kwargs):
+		where_caluse = kwargs
+		self.model_instance.get(where_caluse)
+		return self.model_instance
+
 
 	def all(self):
 		pass
