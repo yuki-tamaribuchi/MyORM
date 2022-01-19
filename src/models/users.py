e024a9d986d12fb22349b74a80c1394e66116b6b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-19 09:04:42 +0900

    Add id column

--- src/models/users.py
+++ src/models/users.py
@@ -3,5 +3,6 @@ from framework.fields import StringField, IntegerField
 
 class Users(Model):
 
+	id = IntegerField(null=False, unique=True, primary_key=True)
 	username = StringField(max_length=30, null=False, unique=True, brank=False)
 	lucky_number = IntegerField(null=True, unique=False)
