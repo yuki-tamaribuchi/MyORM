bc23f9cf97308f0e04a5ead57253237ac8437a35
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-20 00:04:21 +0900

    Create DBConnection

--- src/framework/connection/connect.py
+++ src/framework/connection/connect.py
@@ -6,21 +6,30 @@ from framework.exceptions.connection.base import DatabaseCreatedException
 from settings.databases import DATABASES_LIST
 
 
-def connect():
-	user = DATABASES_LIST["default"]["USER"]
-	password = DATABASES_LIST["default"]["PASSWORD"]
-	host = DATABASES_LIST["default"]["HOST"]
-	database = DATABASES_LIST["default"]["NAME"]
+class DBConnection:
 
-	try:
-		cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
-		return cnx
-	except mysql.connector.Error as err:
-		if err.errno == errorcode.ER_BAD_DB_ERROR:
-			with mysql.connector.connect(user=user, password=password, host=host) as cnx:
-				cursor = cnx.cursor()
-				sql = "CREATE DATABASE {}".format(database)
-				cursor.execute(sql)
-				raise DatabaseCreatedException(database)
-		else:
-			raise err
+	def __new__(cls, *args, **kwargs):
+		if not hasattr(cls, "_instance"):
+			cls._instance = super().__new__(cls)
+		return cls._instance
+
+	def __init__(self):
+		self.user = DATABASES_LIST["default"]["USER"]
+		self.password = DATABASES_LIST["default"]["PASSWORD"]
+		self.host = DATABASES_LIST["default"]["HOST"]
+		self.database = DATABASES_LIST["default"]["NAME"]
+
+
+	def connect(self):
+		try:
+			cnx = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+			return cnx
+		except mysql.connector.Error as err:
+			if err.errno == errorcode.ER_BAD_DB_ERROR:
+				with mysql.connector.connect(user=self.user, password=self.password, host=self.host) as cnx:
+					cursor = cnx.cursor()
+					sql = "CREATE DATABASE {}".format(self.database)
+					cursor.execute(sql)
+					raise DatabaseCreatedException(self.database)
+			else:
+				raise err
