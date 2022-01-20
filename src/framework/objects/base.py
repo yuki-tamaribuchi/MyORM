a8e4680fca343545b1eaf6d78d51ed774c273299
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-21 00:05:40 +0900

    Add sql_dict

--- src/framework/objects/base.py
+++ src/framework/objects/base.py
@@ -1,3 +1,18 @@
 class ObjectsBase(object):
 	def __init__(self, model_instance):
 		self.model_instance = model_instance
+
+		self.sql_dict = {
+			'sql_mode': None,
+			'table': model_instance.__class__.__name__.lower(),
+			'options': None,
+			'insert_columns': None,
+			'insert_values': None,
+			'join': None,
+			'where': None,
+			'group_by': None,
+			'having': None,
+			'order_by': None,
+			'limit': None,
+			'update_sets': None
+		}
