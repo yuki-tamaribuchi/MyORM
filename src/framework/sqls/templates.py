e9b74f281aecdba399475280cb485b3b7ab68da6
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-21 00:02:35 +0900

    Add WHERE_TEMPLATE

--- src/framework/sqls/templates.py
+++ src/framework/sqls/templates.py
@@ -2,5 +2,7 @@ CREATE_TABLE_TEMPLATE = "CREATE TABLE {table} ( {fields} )"
 
 SELECT_TEMPLATE = "SELECT {options} {columns} FROM {table} {join} {where} {order_by} {having} {limit}"
 INSERT_TEMPLATE = "INSERT INTO {table} ({columns}) VALUES ({values})"
-UPDATE_TEMPLATE = "UPDATE {table} SET {sets} {where}"
+UPDATE_TEMPLATE = "UPDATE {table} SET {sets} {where} {order_by} {limit}"
 DELETE_TEMPLATE = "DELETE FROM {table} {where} {order_by} {limit}"
+
+WHERE_TEMPLATE = "WHERE  {where_clause}"
