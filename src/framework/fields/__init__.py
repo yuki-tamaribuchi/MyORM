ea02c52bc2c9a1b4330e9271dbd5af5302d035ca
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-20 13:09:44 +0900

    Add ForeignField

--- src/framework/fields/__init__.py
+++ src/framework/fields/__init__.py
@@ -1,9 +1,11 @@
 from .fields.integer import IntegerField
 from .fields.string import StringField
 from .fields.float import FloadField
+from .fields.foreign import ForeignField
 
 __all__ = [
 	IntegerField,
 	StringField,
 	FloadField,
+	ForeignField
 ]
