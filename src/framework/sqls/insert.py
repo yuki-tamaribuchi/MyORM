28b1b435a0280a7c79178344156bf696162efae5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-23 00:02:07 +0900

    Modify generate_insert_sql

--- src/framework/sqls/insert.py
+++ src/framework/sqls/insert.py
@@ -1,25 +1,16 @@
-from .templates import INSERT_TEMPLATE as template
+from .templates import INSERT_TEMPLATE
 
 
-def generate_insert_sql(model_instance):
-	table_name = model_instance.__class__.__name__.lower()
-	fields_list = model_instance.__dict__["fields_list"]
-
-	insert_colums_arr = []
-	insert_values_arr = []
+def generate_insert_sql(sql_dict):
 	
-	for field_name in fields_list:
-		field_instace = getattr(model_instance, field_name)
-		if field_instace.value is not None:
-			insert_colums_arr.append(field_name)
-			insert_values_arr.append(str(field_instace.value))
-	
-	insert_columns_sql = ", ".join(insert_colums_arr)
-	insert_values_sql = ", ".join(insert_values_arr)
+	columns = ", ".join(sql_dict["insert_columns"])
+	values = ", ".join(sql_dict["insert_values"])
 
-	sql = template.format(
-		table=table_name,
-		columns=insert_columns_sql,
-		values=insert_values_sql
+
+	insert_sql = INSERT_TEMPLATE.format(
+		table=sql_dict["table"],
+		columns=columns,
+		values=values
 	)
-	return sql
+
+	return insert_sql
