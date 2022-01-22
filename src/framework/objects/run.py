599cdd703a3ac58aa36082d0c42928a6b27e6b79
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-23 00:03:04 +0900

    Modify ObjectsRun

--- src/framework/objects/run.py
+++ src/framework/objects/run.py
@@ -1,6 +1,12 @@
 from .base import ObjectsBase
 
+from framework.connection.execute import execute
+from framework.connection.fetch import fetch
+
 from framework.sqls.select import generate_select_sql
+from framework.sqls.insert import generate_insert_sql
+
+from framework.exceptions.objects.base import ResultNotOneException
 
 
 class ObjectsRun(ObjectsBase):
@@ -8,17 +14,43 @@ class ObjectsRun(ObjectsBase):
 
 		if self.sql_dict["sql_mode"]  == "select":
 			sql = generate_select_sql(self.sql_dict)
-
+		elif self.sql_dict["sql_mode"] == "insert":
+			sql = generate_insert_sql(self.sql_dict)
 		else:
 			print('error')
 
 
-		print(sql)
-
 		if self.sql_dict["sql_mode"] == "select":
-			result = None
+			if self.sql_dict["select_mode"] == "get":
+				result = fetch(sql)
+				result_len = len(result)
+				if result_len>1:
+					raise ResultNotOneException
+				elif result_len == 1:
+					model_instances = self.__set_value_to_object(result)
+					return model_instances[0]
+				else:
+					return None
+				
 		else:
-			result = None
+			result = execute(sql)
 
 		return result
+
+
+
+
+	def __set_value_to_object(self, result):
+		model_instances = []
+
+		fields_dict_keys = self.model_instance.fields_dict.keys()
+
+		for r in result:
+			values_dict = {}
+			for i, k in enumerate(fields_dict_keys):
+				values_dict[k] =r[i]
 			
+			self.model_instance.set_values(values_dict, True)
+			model_instances.append(self.model_instance)
+		
+		return model_instances
