b2598444767ce1c0bd7e53554b641203c350b40d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-23 13:34:55 +0900

    Modify generate_columns

--- src/framework/sqls/columns.py
+++ src/framework/sqls/columns.py
@@ -1,4 +1,10 @@
-def generate_columns(columns):
+def generate_columns(columns_dict):
+
+	columns_arr = []
+
+	for k, v in columns_dict.items():
+		for column_name in v:
+			columns_arr.append("{}.{}".format(k, column_name))
 	
-	columns_sql = ", ".join(list(columns))
+	columns_sql = ", ".join(columns_arr)
 	return columns_sql
