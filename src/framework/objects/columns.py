4001a09fda7c3df910ff814740c22a6cc7d2ee01
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-24 00:09:00 +0900

    Modify sql_dict key

--- src/framework/objects/columns.py
+++ src/framework/objects/columns.py
@@ -5,7 +5,7 @@ from framework.exceptions.objects.base import ColumnsNotSpecifiedException
 class ColumnsObject(ObjectsBase):
 	def columns(self, *args):
 		if args:
-			self.sql_dict["columns"] = args
+			self.sql_dict["columns"]["custom"] = args
 			return self
 		else:
 			raise ColumnsNotSpecifiedException
