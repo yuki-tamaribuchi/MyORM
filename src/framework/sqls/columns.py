6d412c07f203c829f309ac8b062cb063998b6378
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-24 00:11:18 +0900

    Modify generate_columns

--- src/framework/sqls/columns.py
+++ src/framework/sqls/columns.py
@@ -1,10 +1,23 @@
-def generate_columns(columns_dict):
+from sqlalchemy import table
 
+
+def generate_columns(objects_instance):
+
+	
 	columns_arr = []
 
-	for k, v in columns_dict.items():
-		for column_name in v:
-			columns_arr.append("{}.{}".format(k, column_name))
-	
+	if objects_instance.sql_dict["columns"]["custom"]:
+		table_name = objects_instance.model_instance.__class__.__name__.lower()
+		for column in objects_instance.sql_dict["columns"]["custom"]:
+			columns_arr.append("{}.{}".format(table_name, column))
+
+	else:
+		column_tables = objects_instance.sql_dict["columns"]
+		column_tables.pop("custom")
+
+		for table_name, columns in column_tables.items():
+			for column in columns:
+				columns_arr.append("{}.{}".format(table_name, column))
+
 	columns_sql = ", ".join(columns_arr)
 	return columns_sql
