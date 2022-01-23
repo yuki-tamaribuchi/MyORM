2d2e116330befd9d41875c07b5bd69436759ce72
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-24 00:10:32 +0900

    Modify sql_dict creation

--- src/framework/objects/select.py
+++ src/framework/objects/select.py
@@ -4,8 +4,9 @@ from .base import ObjectsBase
 class ObjectsSelect(ObjectsBase):
 	def get(self, **kwargs):
 		self.sql_dict["sql_mode"] = "select"
+		self.sql_dict["select_mode"] = "get"
 		self.sql_dict["where"] = kwargs
-
+		self.sql_dict["columns"][self.model_instance.__class__.__name__.lower()] = list(self.model_instance.fields_dict.keys())
 		return self
 
 
