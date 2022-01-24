9f932385c819232d6208e7e3c83d2fbd029538ef
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-25 00:03:41 +0900

    Modify columns temporary

--- src/framework/sqls/columns.py
+++ src/framework/sqls/columns.py
@@ -1,23 +1,30 @@
-from sqlalchemy import table
+from framework.fields.fields.foreign import ForeignField
 
 
 def generate_columns(objects_instance):
 
-	
 	columns_arr = []
 
-	if objects_instance.sql_dict["columns"]["custom"]:
-		table_name = objects_instance.model_instance.__class__.__name__.lower()
-		for column in objects_instance.sql_dict["columns"]["custom"]:
-			columns_arr.append("{}.{}".format(table_name, column))
+	#if objects_instance.sql_dict["columns"]["custom"]:
+	#	table_name = objects_instance.model_instance.__class__.__name__.lower()
+	#	for column in objects_instance.sql_dict["columns"]["custom"]:
+	#		columns_instance = getattr(objects_instance.model_instance, column)
+	#		if isinstance(columns_instance, ForeignField):
+	#			column = column + "_id"
+	#		columns_arr.append("{}.{}".format(table_name, column))
 
-	else:
-		column_tables = objects_instance.sql_dict["columns"]
-		column_tables.pop("custom")
+	#else:
+	#	column_tables = objects_instance.sql_dict["columns"]
+	#	column_tables.pop("custom")
+	#	
+	#	for table_name, columns in column_tables.items():
+	#		for column in columns:
+	#			columns_instance = getattr(objects_instance.model_instance, column)
+	#			if isinstance(columns_instance, ForeignField):
+	#				column = column + "_id"
+	#			columns_arr.append("{}.{}".format(table_name, column))
 
-		for table_name, columns in column_tables.items():
-			for column in columns:
-				columns_arr.append("{}.{}".format(table_name, column))
+	columns_arr.append("*")
 
 	columns_sql = ", ".join(columns_arr)
-	return columns_sql
+	return columns_sql	
