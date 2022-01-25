a2a804e182def4cc1d924c17fc06a2e02f677137
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-26 00:02:25 +0900

    Modify ColumnObject

--- src/framework/objects/columns.py
+++ src/framework/objects/columns.py
@@ -1,11 +1,42 @@
 from .base import ObjectsBase
 
-from framework.exceptions.objects.base import ColumnsNotSpecifiedException
+from framework.fields.fields.foreign import ForeignField
+from framework.exceptions.objects.base import ColumnsNotSpecifiedException, ColumnNotFoundException
 
 class ColumnsObject(ObjectsBase):
 	def columns(self, *args):
 		if args:
-			self.sql_dict["columns"]["custom"] = args
+			columns_arr = self.__create_columns(args)
+			self.__set_columns(columns_arr)
 			return self
 		else:
 			raise ColumnsNotSpecifiedException
+
+	def __create_columns(self, columns):
+		columns_arr = []
+		model_fields_dict = self.sql_dict["table"]().fields_dict
+		for column in columns:
+			columns_name, columns_instance = self.__get_columns_instance(column, model_fields_dict)
+			columns_arr.append((columns_name, columns_instance))
+		return columns_arr
+
+				
+
+	def __get_columns_instance(self, column, model_fields_dict):
+		for field_name, field_instance in model_fields_dict.items():
+			if isinstance(field_instance, ForeignField):
+				field_name = field_name + "_id"
+
+			if column == field_name:
+				return (field_name, field_instance)
+
+		else:
+			raise ColumnNotFoundException
+
+	
+	def __set_columns(self, columns_arr):
+		for i, table in enumerate(self.sql_dict["columns"]):
+			if table["table"] == self.sql_dict["table"]:
+				self.sql_dict["columns"][i]["columns"] = columns_arr
+		return True
+				
