c247f3644d2fb42ab3bc53cf4f51fdf068cf647f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-26 00:06:09 +0900

    Modify generate_join_sql

--- src/framework/sqls/join.py
+++ src/framework/sqls/join.py
@@ -1,25 +1,29 @@
 from .templates import JOIN_TEMPLATE
 
+from framework.fields.fields.foreign import ForeignField
+
 def generate_join_sql(join_arr):
-	right_table_arr = []
+	right_table_name_arr = []
 	condition_arr = []
 
 	for join in join_arr:
-		right_table_arr.append(join["right_table"])
-		condition_arr.append(
-			"{left_table}.{column} = {right_table}.id".format(
-				left_table=join["left_table"],
-				column=join["column"],
-				right_table=join["right_table"]
-			)
-		)
+
+		for k, v in join["left_table"]().fields_dict.items():
+			if isinstance(v, ForeignField) and v.relation_model == join["right_table"]:
+				left_table_column = k + "_id"
+
+		left_table_name = join["left_table"].__name__.lower()
+		right_table_name = join["right_table"].__name__.lower()
+
+		right_table_name_arr.append(right_table_name)
+		condition_arr.append("{}.{} = {}.id".format(left_table_name, left_table_column, right_table_name))
+
 	
 	join_sql = ""
 
-	for i in range(len(right_table_arr)):
+	for i in range(len(right_table_name_arr)):
 		join_sql += JOIN_TEMPLATE.format(
-			table=right_table_arr[i],
+			table=right_table_name_arr[i],
 			condition=condition_arr[i]
 		)
-
 	return join_sql
