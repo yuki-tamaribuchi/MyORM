62e3391fc2af1e769430a09c51d109589539f578
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-26 00:08:03 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from platformdirs import user_cache_dir
 from models.entry import Entry
 from models.users import Users
 
@@ -10,57 +11,59 @@ from models.users import Users
 
 
 
-
 #動作確認済分
 
 #テーブル作成 (作成するテーブルをmodel.__init__内のmodelsリストに追加しておく)
 #from framework.operates.create_table import create_all_table
 #create_all_table()
 
-
-
-#以下SQL発行は未実装
-
-#直接渡してデータをセットし、保存する方法
-#user=Users(username="yuki")
-#user.save()
-#print(user)
-
-
-#createメソッドにデータを渡し保存する方法
+#createメソッドで作成
 #user = Users().objects.create(username="yuki", lucky_number=1)
-#print(user)
-
-
-#インスタンスのupdateメソッドで値を更新
-#user = Users().objects.create(username="yuki", lucky_number=1)
-#user.objects.update(lucky_number = 2)
-
-#インスタンスのdeleteメソッドで削除
-#user = Users().objects.create(username="yuki", lucky_number=1)
-#user.objects.delete()
 
 
 #インスタンスのgetメソッドでデータを取得
-#Users().objects.create(username="yuki")
-#user = Users().objects.get(username="yuki")
+#user = Users().objects.get(username="yuki").run()
+#print(user)
 
 
-
-
-#Users().objects.get(username="yuki", lucky_number=1).run()
-
-#getのみ、OK
-#user= Users().objects.get(username="yuki", lucky_number=1).run()
-#print(user.username)
-
-#get + カラム指定、エラー
+#get + カラム指定
 #user=Users().objects.get(username="yuki", lucky_number=1).columns("username", "lucky_number").run()
 #print(user)
 
 
-#Users().objects.create(username="yuki", lucky_number=1).run()
+user = Users().objects.filter({"username":"yuki"},{"username":"sample"}).run()
+print(user)
 
 
-entry = Entry().objects.get(title="sample").select_related("writer").run()
-print(entry)
+#ANDでフィルター
+#entry = Entry().objects.filter(writer="3", title="sample").run()
+#print(entry)
+
+
+
+#関連テーブルも同時にget
+#entry = Entry().objects.get(title="sample").select_related("writer").run()
+#entry = Entry().objects.get(title="sample").select_related("writer").columns("title").run()
+#print(entry)
+
+
+
+
+
+
+
+
+
+
+#以下SQL発行は未実装
+
+
+#インスタンスのupdateメソッドで値を更新
+#user = Users().objects.create(username="yuki", lucky_number=1)
+#Users().objects.update(lucky_number = 2).filter(username="yuki", lucky_number=1).run()
+
+#Users().objects.update(lucky_number = 2).filter({"username":"yuki"},{"username":"sample"}).run()
+
+
+#インスタンスのdeleteメソッドで削除
+#user.objects.delete()
