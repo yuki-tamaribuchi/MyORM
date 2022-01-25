aa94329699679521580767e6ac32d39c9e8cd866
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-26 00:05:11 +0900

    Modify generate_columns

--- src/framework/sqls/columns.py
+++ src/framework/sqls/columns.py
@@ -5,26 +5,15 @@ def generate_columns(objects_instance):
 
 	columns_arr = []
 
-	#if objects_instance.sql_dict["columns"]["custom"]:
-	#	table_name = objects_instance.model_instance.__class__.__name__.lower()
-	#	for column in objects_instance.sql_dict["columns"]["custom"]:
-	#		columns_instance = getattr(objects_instance.model_instance, column)
-	#		if isinstance(columns_instance, ForeignField):
-	#			column = column + "_id"
-	#		columns_arr.append("{}.{}".format(table_name, column))
+	table_columns = objects_instance.sql_dict["columns"]
+	for table in table_columns:
+		table_name = table["table"].__name__.lower()
+		columns = table["columns"]
 
-	#else:
-	#	column_tables = objects_instance.sql_dict["columns"]
-	#	column_tables.pop("custom")
-	#	
-	#	for table_name, columns in column_tables.items():
-	#		for column in columns:
-	#			columns_instance = getattr(objects_instance.model_instance, column)
-	#			if isinstance(columns_instance, ForeignField):
-	#				column = column + "_id"
-	#			columns_arr.append("{}.{}".format(table_name, column))
-
-	columns_arr.append("*")
-
+		for column_name ,column_instance in columns:
+			if isinstance(column_instance, ForeignField):
+				column_name = column_name + "_id"
+			columns_arr.append("{}.{}".format(table_name, column_name))
+		
 	columns_sql = ", ".join(columns_arr)
 	return columns_sql	
