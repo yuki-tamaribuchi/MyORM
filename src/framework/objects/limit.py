7082e125d159cff4eb97536435bd120b9f92365e
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-27 00:03:11 +0900

    Create ObjectsLimit class

---
+++ src/framework/objects/limit.py
@@ -0,0 +1,13 @@
+from .base import ObjectsBase
+
+from framework.exceptions.objects.base import LimitNumberMustBePositiveException
+
+
+class ObjectsLimit(ObjectsBase):
+	def limit(self, n:int):
+		if n>0:
+			self.sql_dict["limit"] = n
+			return self
+
+		else:
+			raise LimitNumberMustBePositiveException
