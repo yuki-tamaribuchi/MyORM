848e0e29324c018097bde9bd1428c8de75647b3a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-27 00:07:27 +0900

    Modify filter_arr

--- src/framework/sqls/where.py
+++ src/framework/sqls/where.py
@@ -7,8 +7,8 @@ def generate_where_sql(all_filter_conditon_arr):
 
 	for filter_condition in all_filter_conditon_arr:
 		for k, v in filter_condition.items():
-			filter_arr.append("{} = \"{}\"".format(k, v["field_value"]))
-	
+			filter_arr.append("{}.{} = \"{}\"".format(v["table_name"], k, v["field_value"]))
+
 
 	filter_sql = " AND ".join(filter_arr)
 	
