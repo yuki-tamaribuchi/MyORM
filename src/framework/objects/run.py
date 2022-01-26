e9d08bd930089ec9a0c41c2a98e26ef0f247168c
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-27 00:05:40 +0900

    Add delete and update mode

--- src/framework/objects/run.py
+++ src/framework/objects/run.py
@@ -1,10 +1,11 @@
 from .base import ObjectsBase
-
 from framework.connection.execute import execute
 from framework.connection.fetch import fetch
 
 from framework.sqls.select import generate_select_sql
 from framework.sqls.insert import generate_insert_sql
+from framework.sqls.delete import generate_delete_sql
+from framework.sqls.update import generate_update_sql
 
 from framework.exceptions.objects.base import ResultNotOneException
 
@@ -16,6 +17,10 @@ class ObjectsRun(ObjectsBase):
 			sql = generate_select_sql(self)
 		elif self.sql_dict["sql_mode"] == "insert":
 			sql = generate_insert_sql(self.sql_dict)
+		elif self.sql_dict["sql_mode"] == "delete":
+			sql = generate_delete_sql(self.sql_dict)
+		elif self.sql_dict["sql_mode"] == "update":
+			sql = generate_update_sql(self.sql_dict)
 		else:
 			print('error')
 
@@ -31,27 +36,10 @@ class ObjectsRun(ObjectsBase):
 
 				else:
 					return None
-			elif self.sql_dict["select_mode"] == "filter":
+
+			elif self.sql_dict["select_mode"] in ["filter", "all"]:
 				result = fetch(sql)
 				return result
 		else:
 			result = execute(sql)
 		return result
-
-
-
-
-	def __set_value_to_object(self, result):
-		model_instances = []
-
-		fields_dict_keys = self.model_instance.fields_dict.keys()
-
-		for r in result:
-			values_dict = {}
-			for i, k in enumerate(fields_dict_keys):
-				values_dict[k] =r[i]
-			
-			self.model_instance.set_values(values_dict, True)
-			model_instances.append(self.model_instance)
-		
-		return model_instances
