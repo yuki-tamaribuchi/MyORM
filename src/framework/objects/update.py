c72bd6dfd8c94b8f41ed3a994718572a471c9ccc
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-27 00:11:25 +0900

    Modify update

--- src/framework/objects/update.py
+++ src/framework/objects/update.py
@@ -1,10 +1,25 @@
 from .base import ObjectsBase
 
+from framework.exceptions.objects.base import UpdateDataNotSpecifiedException
 
 
 class ObjectsUpdate(ObjectsBase):
 	def update(self, **kwargs):
-		update_data = kwargs
-		self.model_instance.update_values(update_data)
-		self.model_instance.save()
-		return self.model_instance
+		if kwargs:
+
+			update_data= kwargs
+
+			update_sets_arr= []
+
+			for k, v in update_data.items():
+				update_sets_arr.append(
+					"{} = {}".format(k, v)
+				)
+
+			self.sql_dict["update_sets"] = update_sets_arr
+			self.sql_dict["sql_mode"] = "update"
+
+			return self
+
+		else:
+			raise UpdateDataNotSpecifiedException
