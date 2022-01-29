25b7d91017227958aad9d50daab619bf45dc7d0b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-30 00:02:35 +0900

    Create Exceptions

--- src/framework/exceptions/objects/base.py
+++ src/framework/exceptions/objects/base.py
@@ -1,2 +1,41 @@
 class ColumnsNotSpecifiedException(Exception):
 	pass
+
+class ColumnNotFoundException(Exception):
+	pass
+
+class CreateDataNotFoundException(Exception):
+	pass
+
+class FieldNotInThisClassException(Exception):
+	pass
+
+class ResultNotOneException(Exception):
+	pass
+
+class SelectRelatedTableNotFoundException(Exception):
+	pass
+
+class UpdateDataNotSpecifiedException(Exception):
+	pass
+
+class FilterConditionNotSpecifiedException(Exception):
+	pass
+
+class FilterColumnNotFoundException(Exception):
+	pass
+
+class GetConditonNotSpecifiedException(Exception):
+	pass
+
+class GetColumnNotFoundException(Exception):
+	pass
+
+class UpdateSetsNotFoundException(Exception):
+	pass
+
+class OrderByKeyNotSpecifiedException(Exception):
+	pass
+
+class LimitNumberMustBePositiveException(Exception):
+	pass
