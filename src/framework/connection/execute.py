6198a062fae66e1f094e01c6e25de1b89c3f72e4
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-30 00:02:05 +0900

    Modify return when insert

--- src/framework/connection/execute.py
+++ src/framework/connection/execute.py
@@ -3,14 +3,19 @@ from mysql.connector import errorcode
 
 from .connect import DBConnection
 
-def execute(sql)->bool:
+def execute(sql, is_insert)->bool:
 	db = DBConnection()
 
 	with db.connect() as cnx:
 		with cnx.cursor() as cursor:
 			try:
 				cursor.execute(sql)
-				return True
+				cnx.commit()
+
+				if is_insert:
+					return cursor.lastrowid
+				else:
+					return True
 			
 			except	mysql.connector.Error as err:
 				if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
