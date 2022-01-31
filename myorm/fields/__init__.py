4af9b1fbb9fdde5dc776e89ce68a3c4b86e67e46
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-31 11:45:09 +0900

    Add DatetimeField

--- myorm/fields/__init__.py
+++ myorm/fields/__init__.py
@@ -2,10 +2,12 @@ from .fields.integer import IntegerField
 from .fields.string import StringField
 from .fields.float import FloadField
 from .fields.foreign import ForeignField
+from .fields.datetime import DatetimeField
 
 __all__ = [
 	IntegerField,
 	StringField,
 	FloadField,
-	ForeignField
+	ForeignField,
+	DatetimeField
 ]
