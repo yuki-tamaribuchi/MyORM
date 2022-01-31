b33e19922a23e359ef841dfe73c3111952a23552
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-01-31 11:44:39 +0900

    Modify for DatetimeField

--- myorm/objects/create.py
+++ myorm/objects/create.py
@@ -1,40 +1,48 @@
+from datetime import datetime
+
 from .base import ObjectsBase
 
 from myorm.fields.fields.foreign import ForeignField
+from myorm.fields.fields.datetime import DatetimeField
 from myorm.exceptions.objects.base import CreateDataNotFoundException, FieldNotInThisClassException
 
 
 class ObjectsCreate(ObjectsBase):
-	def create(self, **kwargs):		
-		if not kwargs:
-			raise CreateDataNotFoundException
+	def create(self, **kwargs):
 
 		insert_columns_arr = []
 		insert_values_arr = []
 
-
-		self.sql_dict["sql_mode"] = "insert"
+		insert_data = kwargs
 		model_fields_dict = self.model_instance.fields_dict
-		insert_data =kwargs
-		for k, v in insert_data.items():
-			if k not in model_fields_dict:
+
+
+		for field_name in insert_data.keys():
+			if field_name not in model_fields_dict.keys():
 				raise FieldNotInThisClassException
-			
-
-			if model_fields_dict[k].validate(v):
-				insert_values_arr.append("\"" + str(v) + "\"")
-
-				if isinstance(model_fields_dict[k], ForeignField):
-					insert_columns_arr.append(k + "_id")
-				else:
-					insert_columns_arr.append(k)
-
 
 		
+		for field_name, field_instance in model_fields_dict.items():
+			if isinstance(field_instance, DatetimeField):
+				if field_name not in insert_data and not field_instance.null:
+					if field_instance.auto_add:
+						insert_data["datetime"] = datetime.now()
+					else:
+						insert_data["datetime"] = None
+
+
+			if field_name in insert_data:
+				if field_instance.validate(insert_data[field_name]):
+					insert_values_arr.append(insert_data[field_name])
+
+					if isinstance(field_instance, ForeignField):
+						field_name = field_name + "_id"
+
+					insert_columns_arr.append(field_name)
+		
+
+		self.sql_dict["sql_mode"] = "insert"
 		self.sql_dict["insert_columns"] = insert_columns_arr
 		self.sql_dict["insert_values"] = insert_values_arr
-
-
-
 
 		return self
