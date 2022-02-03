50288cada7d54978b5974a8eaba628daefcd176a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-04 00:05:04 +0900

    Add process to DatatimeField auto update

--- myorm/objects/update.py
+++ myorm/objects/update.py
@@ -5,6 +5,9 @@ from myorm.exceptions.objects.base import UpdateDataNotSpecifiedException
 
 class ObjectsUpdate(ObjectsBase):
 	def update(self, **kwargs):
+		from myorm.fields.fields.datetime import DatetimeField
+		from datetime import datetime
+
 		if kwargs:
 
 			update_data= kwargs
@@ -15,6 +18,13 @@ class ObjectsUpdate(ObjectsBase):
 				update_sets_arr.append(
 					"{} = \"{}\"".format(k, v)
 				)
+
+			for k, v in self.model_instance.fields_dict.items():
+				if isinstance(v, DatetimeField) and v.auto_update:
+					update_sets_arr.append(
+						"{} = \"{}\"".format(k, datetime.now())
+					)
+
 
 			self.sql_dict["update_sets"] = update_sets_arr
 			self.sql_dict["sql_mode"] = "update"
