95cd056dafba82d82547586206b1065bc340d1c7
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-06 00:15:07 +0900

    Modify execute call

--- myorm/operates/create_table.py
+++ myorm/operates/create_table.py
@@ -1,12 +1,12 @@
 from myorm.sqls.create_table import generate_create_table_sql
 from myorm.connection.execute import execute
 
-from models import models
+from src.models import models
 
 
 def create_all_table():
 	for model in models:
 		sql = generate_create_table_sql(model)
-		execute(sql)
+		execute(sql, False)
 	return True
 		
